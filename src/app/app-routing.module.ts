20a77a1c700936a0593a71ca067e64dc45335db9
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 22:29:05 -0500

    Funcionando el routerLink para mostrar los componetes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListadoPuestosComponent } from "./componentes/listado-puestos/listado-puestos.component";
 
 const routes: Routes = [
-  {path: '',pathMatch:'full',redirectTo:"listado-puestos"},
   {path:"puestos", component: ListadoPuestosComponent}
 ];
 
