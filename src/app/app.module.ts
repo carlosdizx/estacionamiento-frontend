c569cc35d8d9b12994c21f5b01ff14cff2fb582b
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 21:23:26 -0500

    Instalado angular material y creado componente con la barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoPuestosComponent } from './componentes/listado-puestos/listado-puestos.component';
+import { ToolbarComponent } from './basicos/toolbar/toolbar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListadoPuestosComponent
+    ListadoPuestosComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
