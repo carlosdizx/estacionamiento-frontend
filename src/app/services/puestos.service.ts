4387c058b1d67a07ef8e7236069235c79797fce6
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 23:09:43 -0500

    Creado en el service el metodo de listar los datos de los puestos

--- src/app/services/puestos.service.ts
+++ src/app/services/puestos.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Puesto } from './Puesto';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PuestosService {
+  //const puestos : Puesto[] = new Puesto[];
 
-  constructor() { }
+  API: string = 'http://127.0.0.1:8000/api/puestos';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  listarPuestos():Observable<any>{
+    return this.http.get(this.API);
+  }
 }
