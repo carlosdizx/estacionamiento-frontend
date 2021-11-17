a91f17346e5a6d3d8a1f577c02cfb3e3cc8f50d3
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 22:49:23 -0500

    Importado FormsModule, y HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,25 @@ import { AppComponent } from './app.component';
 import { ListadoPuestosComponent } from './componentes/listado-puestos/listado-puestos.component';
 import { ToolbarComponent } from './basicos/toolbar/toolbar.component';
 
-import {MatChipsModule} from "@angular/material/chips";
-import {MatButtonModule} from "@angular/material/button";
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListadoPuestosComponent,
-    ToolbarComponent
-  ],
+  declarations: [AppComponent, ListadoPuestosComponent, ToolbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatChipsModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
