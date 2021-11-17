f61fd8bf296b8e404634d2d8d09443910cefcd9e
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 21:18:58 -0500

    Agregada la ruta para listar empleados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoPuestosComponent } from "./componentes/listado-puestos/listado-puestos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',pathMatch:'full',redirectTo:"listado-puestos"},
+  {path:"puestos", component: ListadoPuestosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
