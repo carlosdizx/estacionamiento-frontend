2dfd052191f018a648bc26b655b88a912c2a9355
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 10:31:01 -0500

    Preparado para una vez se 'saque' o desocupe el puesto se registre esa info en el api de registros

--- src/app/componentes/listado-puestos/listado-puestos.component.ts
+++ src/app/componentes/listado-puestos/listado-puestos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Puesto } from 'src/app/services/Puesto';
+import { Registro } from 'src/app/services/Registro';
 import { PuestosService } from '../../services/puestos.service';
+import { RegistrosService } from '../../services/registros.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +13,11 @@ import { Router } from '@angular/router';
 export class ListadoPuestosComponent implements OnInit {
   puestos: Puesto[] = [];
 
-  constructor(private service: PuestosService, private router: Router) {}
+  constructor(
+    private service: PuestosService,
+    private serviceAux: RegistrosService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.listarPuestos();
@@ -24,7 +30,21 @@ export class ListadoPuestosComponent implements OnInit {
   }
 
   async actualizarPuesto(puesto: Puesto) {
+    const actual = new Date();
     if (puesto.placa !== '1') {
+      const registro: Registro = new Registro();
+      registro.placa = puesto.placa;
+      registro.propietario = puesto.propietario;
+      registro.inicio = puesto.inicio;
+      registro.fin =
+        actual.getFullYear() +
+        '-' +
+        (actual.getMonth() + 1) +
+        '-' +
+        actual.getDate() +
+        '-' +
+        actual.toLocaleTimeString().split(' ')[0];
+      console.log(registro);
       puesto.placa = '1';
       puesto.propietario = '';
       this.service.actualizarPuesto(puesto).subscribe();
