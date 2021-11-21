7769e2b9ab2630cc4ff66a8c665ea632c7989f63
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 09:02:53 -0500

    Componente del listado de los registros se agrego en la ruta y se lo muestra en pantalla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListadoPuestosComponent } from './componentes/listado-puestos/listado-puestos.component';
 import { EditarPuestoComponent } from './componentes/editar-puesto/editar-puesto.component';
+import {ListadoRegistrosComponent} from "./componentes/listado-registros/listado-registros.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'puestos', pathMatch: 'full' },
   { path: 'puestos', component: ListadoPuestosComponent },
   { path: 'puestos/:id', component: EditarPuestoComponent },
+  { path: 'registros', component: ListadoRegistrosComponent },
 ];
 
 @NgModule({
